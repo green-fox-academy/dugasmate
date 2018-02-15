22dc03bdd646c7151668d352de6ad727ed0ba5fb
dugasmate <dugasmate@gmail.com>
2018-02-15 23:00:32 +0100

    Uploaded IWontCheat with easter egg

---
+++ week-01/day-3/IWontCheat/IWontCheat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+            int counter = 1;
+            Console.WriteLine(counter + ". I won't cheat on the exam!");
+            for (int i = 0; i < 99; i++)
+            {
+                counter = counter + 1;
+                Console.WriteLine(counter + ". I won't cheat on the exam!");
+
+            }
+            if (counter == 101)
+            {
+                Console.WriteLine("101. Lie, Cheat, Steal!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
