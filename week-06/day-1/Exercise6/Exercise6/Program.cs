0aa587450333cb8032f6b0675ab0e406a1356e26
dugasmate <dugasmate@gmail.com>
2018-03-26 14:26:57 +0200

    Exercise 6 added

---
+++ week-06/day-1/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string daString = "It is what it is";
+            var frequency = daString.ToLower().GroupBy(x => x).Select(x => x.Count());
+            foreach (var cha in frequency)
+            {
+                Console.WriteLine(cha);
+            }
+            Console.ReadLine();
+        }
+    }
+}
