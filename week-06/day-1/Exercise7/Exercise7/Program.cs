59b12cc1a46654c8bd4e9487b9679d1cb551ca79
dugasmate <dugasmate@gmail.com>
2018-03-26 14:33:33 +0200

    added Exercise7

---
+++ week-06/day-1/Exercise7/Exercise7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+            var theWord = cities.Where(x => x.StartsWith("A") && x.EndsWith("I"));
+            foreach (var cha in theWord)
+            {
+                Console.WriteLine(cha);
+            }
+            Console.ReadLine();
+        }
+    }
+}
