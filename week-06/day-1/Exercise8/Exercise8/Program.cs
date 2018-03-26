dfa990f1788068b80766f312a711f0ae1a4abf62
dugasmate <dugasmate@gmail.com>
2018-03-26 14:40:33 +0200

    Exercise 8 added

---
+++ week-06/day-1/Exercise8/Exercise8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the uppercase characters in a string!
+            string daSTring = "NAMIVaaaaAanN?";
+            var upper = daSTring.Where(x => char.IsUpper(x));
+            foreach (var letters in upper)
+            {
+                Console.WriteLine(letters);
+            }
+            Console.ReadLine();
+        }
+    }
+}
