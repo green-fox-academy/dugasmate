e93a2bcb89d3bc98b09dfe38320e1d07a1a17027
dugasmate <dugasmate@gmail.com>
2018-03-26 15:27:55 +0200

    Exercise10 added

---
+++ week-06/day-1/Exercise10/Exercise10/Fox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise10
+{
+    class Fox
+    {
+        public string name, type, color;
+        public Fox (string name, string type, string color)
+        {
+            this.name = name;
+            this.type = type;
+            this.color = color;
+        }
+    }
+}
---
+++ week-06/day-1/Exercise10/Exercise10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fox> foxes = new List<Fox>
+            {
+                new Fox("Arno", "Palida", "Green"),
+                new Fox("Dani", "Other", "Brown"),
+                new Fox("Nope", "Other", "Green"),
+                new Fox("Mate", "Other", "Black"),
+                new Fox("Blanka", "Palida", "Green")
+            };
+            var greenfoxes = foxes.Where(y => y.color == "Green");
+            foreach (var green in greenfoxes)
+            {
+                Console.WriteLine(green.name + " is a " + green.color + " fox");
+            }
+            var greenPalidas = foxes.Where(y => y.color == "Green" && y.type == "Palida");
+            foreach (var palida in greenPalidas)
+            {
+                Console.WriteLine(palida.name + " is a " + palida.color + " fox");
+            }
+            Console.ReadLine();
+        }
+    }
+}
