03bb498df223d85ccae9b16d8b74ec6236360212
dugasmate <dugasmate@gmail.com>
2018-03-26 14:58:09 +0200

    Exercise 9 added

---
+++ week-06/day-1/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArray = new[] { 'N', 'a', 'm', 'i' };
+            string joined = String.Join("", charArray);
+            Console.WriteLine(joined);
+            Console.ReadLine();
+        }
+    }
+}
