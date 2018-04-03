a5ec29e38b428380e6b8d4405dce42d758937ec7
dugasmate <dugasmate@gmail.com>
2018-04-03 14:48:01 +0200

    csöhötömölö added

---
+++ week-07/Elso/Elso/Controllers/WebController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elso.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elso.Controllers
+{
+
+        [Route("web")]
+        public class WebController : Controller
+        {
+            [Route("greeting")]
+            public IActionResult Greeting()
+            {
+            var greeting = new Greeting("World");
+                return View(greeting);
+            }
+
+        }
+    
+}
