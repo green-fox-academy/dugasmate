988afbc5ef23ce36f0224fe463475194271d67b9
dugasmate <dugasmate@gmail.com>
2018-04-05 12:52:19 +0200

    Counter added

---
+++ week-07/day-3/Counter/Counter/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Counter.Models;
+
+namespace Counter.Controllers
+{
+    public class HomeController : Controller
+    {
+        ICountable countPlz;
+        public HomeController (ICountable countPlz)
+        {
+            this.countPlz = countPlz;
+        }
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View(countPlz);
+        }
+        [HttpPost]
+        [Route("Same")]
+        public IActionResult Same()
+        {
+            countPlz.Count();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ week-07/day-3/Counter/Counter/Models/Counterke.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Models
+{
+    public class Counterke : ICountable
+    {
+        public int number { get; set; }
+        public Counterke (int number = 0)
+        {
+            this.number = number;
+        }
+        public void Count()
+        {
+            number++;
+        }
+    }
+}
