8a365f301042240d544e3fc304dd1757302207c4
dugasmate <dugasmate@gmail.com>
2018-04-05 15:32:40 +0200

    Anagramm added

---
+++ week-07/day-3/Anagramm/Anagramm/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Anagramm.Models;
+
+namespace Anagramm.Controllers
+{
+    public class HomeController : Controller
+    {
+        IAnagrammable anagrammCheck;
+        public HomeController (IAnagrammable anagrammCheck)
+        {
+            this.anagrammCheck = anagrammCheck;
+        }
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Check")]
+        public IActionResult Check(string firstWord, string secondWord)
+        {
+            return View(anagrammCheck.IsAnagramm(firstWord, secondWord));
+        }
+    }
+}
---
+++ week-07/day-3/Anagramm/Anagramm/Models/Anagramma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Anagramm.Models
+{
+    public class Anagramma : IAnagrammable
+    {
+        public bool IsAnagramm(string firstWord, string secondWord)
+        {
+            firstWord = firstWord.ToUpper();
+            secondWord = secondWord.ToUpper();
+            char[] arrFirstWord = firstWord.ToCharArray();
+            char[] arrSecondWord = secondWord.ToCharArray();
+            Array.Sort(arrFirstWord);
+            Array.Sort(arrSecondWord);
+            for (int i = 0; i < arrFirstWord.Length; i++)
+            {
+                if (arrFirstWord[i] != arrSecondWord[i])
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
